e7649b7f36499b63cc449d295d33f513a8bc8825
SixtenGedda <sixten.gedda@gmail.com>
2025-11-15 20:18:09 +0100

    Add task struct and args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::env;
+use std::fs;
+use serde::{Deserialize, Serialize};
+use serde_json;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let task_desc = &args[2];
+
+    struct Task {
+        id: i32,
+        description: String,
+        status: String,
+        created_at: String,
+        updated_at: String,
+    }
+
+    fn create_id {
+
+    }
+
+      match args[1].as_str() {
+        "list" => {
+            let contents =
+                fs::read_to_string("tasks.json").expect("Should have been able to read the file");
+            println!("{contents}")
+        }
+        "add" => {
+            fs::write("tasks.json", task).expect("Unable to write file");
+        }
+        _ => {
+            println!("unknown command. try again")
+        }
+    }
 }
