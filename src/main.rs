035bd72b206aec8689f88fb24d362b3886573b21
SixtenGedda <sixten.gedda@gmail.com>
2025-11-22 17:12:30 +0100

    add mark-in-progress and done functionality

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use chrono::Local;
 use serde::{Deserialize, Serialize};
-use serde_json;
 use std::env;
 use std::fs;
 
@@ -13,6 +12,16 @@ struct Task {
     updated_at: String,
 }
 
+fn create_task(tasks: &Vec<Task>, new_desc: String) -> Task {
+    Task {
+        id: create_id(tasks),
+        description: new_desc,
+        status: "todo".to_string(),
+        created_at: task_create_datetime(),
+        updated_at: task_create_datetime(),
+    }
+}
+
 fn create_id(tasks: &Vec<Task>) -> u32 {
     let mut new_id: u32 = 0;
     for task in tasks {
@@ -23,19 +32,13 @@ fn create_id(tasks: &Vec<Task>) -> u32 {
     new_id + 1
 }
 
-fn create_task(tasks: &Vec<Task>, new_desc: String) -> Task {
-    let newtask = Task {
-        id: create_id(tasks),
-        description: new_desc,
-        status: "todo".to_string(),
-        created_at: task_create_datetime(),
-        updated_at: task_create_datetime(),
-    };
-    newtask
-}
-
 fn task_create_datetime() -> String {
     Local::now().format("%Y-%m-%dT%H:%M:%S").to_string()
+}
+
+fn save_tasks(tasks: &Vec<Task>) {
+    let json = serde_json::to_string_pretty(&tasks).expect("failed to add task to list");
+    fs::write("tasks.json", json).expect("failed to write to file");
 }
 
 fn main() {
@@ -60,33 +63,52 @@ fn main() {
         }
         "add" => {
             let new_desc = &args[2..].join(" ");
-            let new_task = create_task(&tasks, new_desc.to_string());
+            let new_task = create_task(&tasks, new_desc.clone());
             tasks.push(new_task);
-            let json = serde_json::to_string_pretty(&tasks).expect("failed to add task to list");
-            fs::write("tasks.json", json).expect("failed to write to file");
+            save_tasks(&tasks);
         }
         "update" => {
-            let task_update_desc: &String = &args[3];
+            let task_update_desc: &String = &args[3..].join(" ");
             let update_id: u32 = args[2].parse().expect("Task ID needs to be a number");
             for task in &mut tasks {
                 if task.id == update_id {
-                    task.description = task_update_desc.to_string();
-                    task.updated_at = task_create_datetime()
+                    task.description = task_update_desc.clone();
+                    task.updated_at = task_create_datetime();
+                    break;
                 }
             }
-            let json =
-                serde_json::to_string_pretty(&tasks).expect("failed to update json task list");
-            fs::write("tasks.json", json).expect("failed to write to file");
+            save_tasks(&tasks);
         }
         "remove" => {
             let delete_id: u32 = args[2].parse().expect("Task ID needs to be a number");
             tasks.retain(|task| task.id != delete_id);
-            let json =
-                serde_json::to_string_pretty(&tasks).expect("failed to update json task list");
-            fs::write("tasks.json", json).expect("failed to write to file");
+            save_tasks(&tasks);
         }
+        "mark-in-progress" => {
+            let update_id: u32 = args[2].parse().expect("Task ID needs to be a number");
+            for task in &mut tasks {
+                if task.id == update_id {
+                    task.status = "in-progress".to_string();
+                    task.updated_at = task_create_datetime();
+                    break;
+                }
+            }
+            save_tasks(&tasks);
+        }
+        "mark-done" => {
+            let update_id: u32 = args[2].parse().expect("Task ID needs to be a number");
+            for task in &mut tasks {
+                if task.id == update_id {
+                    task.status = "done".to_string();
+                    task.updated_at = task_create_datetime();
+                    break;
+                }
+            }
+            save_tasks(&tasks);
+        }
+
         _ => {
-            println!("unknown command. try again")
+            println!("unknown command. try again");
         }
     }
 }
