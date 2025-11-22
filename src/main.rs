03aed0922634ef01b0222c347740ebdacc8d0e03
SixtenGedda <sixten.gedda@gmail.com>
2025-11-22 17:51:19 +0100

    add tasks.json to gitignore and remove tests

--- src/main.rs
+++ src/main.rs
@@ -117,18 +117,3 @@ fn main() {
         }
     }
 }
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn add_task() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
