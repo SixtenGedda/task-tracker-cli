471079e355b95c3f6974f2699b126365073d8522
SixtenGedda <sixten.gedda@gmail.com>
2026-01-21 19:27:26 +0100

    refactor: move logic from main to Tasks impl block

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,105 @@ struct Task {
     updated_at: String,
 }
 
-fn create_task(tasks: &Vec<Task>, new_desc: String) -> Task {
-    Task {
-        id: create_id(tasks),
-        description: new_desc,
-        status: "todo".to_string(),
-        created_at: task_create_datetime(),
-        updated_at: task_create_datetime(),
+struct Tasks {
+    tasks: Vec<Task>,
+    path: String,
+}
+
+impl Tasks {
+    fn load(path: String) -> Tasks {
+        if !std::path::Path::new(&path).exists() {
+            fs::write(&path, "[]").expect(&format!("Could not create {}", path));
+        }
+
+        let contents: Vec<Task> = serde_json::from_str(
+            fs::read_to_string("tasks.json")
+                .expect("cant read file")
+                .as_str(),
+        )
+        .expect("could not parse JSON");
+
+        return Tasks {
+            tasks: contents,
+            path,
+        };
+    }
+
+    fn create(&mut self, args: Vec<String>) {
+        let new_desc = args[2..].join(" ");
+        let task = Task {
+            id: create_id(&self.tasks),
+            description: new_desc,
+            status: "todo".to_string(),
+            created_at: now_datetime(),
+            updated_at: now_datetime(),
+        };
+        self.tasks.push(task);
+        self.save();
+    }
+
+    fn update(&mut self, args: Vec<String>) {
+        let update_desc: String = args[3..].join(" ");
+        let update_id: u32 = args[2].parse().expect("Task ID needs to be a number");
+        let index = self.tasks.iter_mut().position(|task| task.id == update_id);
+        match index {
+            Some(i) => {
+                self.tasks[i].description = update_desc;
+                self.tasks[i].updated_at = now_datetime();
+                self.save();
+                println!("Task updated");
+            }
+            None => {
+                println!("Task not found");
+            }
+        }
+    }
+
+    fn save(&self) {
+        let json =
+            serde_json::to_string_pretty(&self.tasks).expect("failed to convert list to json");
+        fs::write(&self.path, json).expect("failed to write to file");
+    }
+
+    fn delete(&mut self, args: Vec<String>) {
+        let delete_id: u32 = args[2].parse().expect("Task ID needs to be a number");
+        self.tasks.retain(|task| task.id != delete_id);
+        self.save();
+    }
+
+    fn list(&self, args: Vec<String>) {
+        for task in &self.tasks {
+            /*check if there are more than one
+            argument before trying to access
+            index 2 */
+            if args.len() <= 2 || args[2] == task.status {
+                println!("{} | {} | {}", task.id, task.description, task.status);
+            }
+        }
+    }
+
+    fn mark_in_progress(&mut self, args: Vec<String>) {
+        let update_id: u32 = args[2].parse().expect("Task ID needs to be a number");
+        for task in &mut self.tasks {
+            if task.id == update_id {
+                task.status = "in-progress".to_string();
+                task.updated_at = now_datetime();
+                break;
+            }
+        }
+        self.save();
+    }
+
+    fn mark_done(&mut self, args: Vec<String>) {
+        let update_id: u32 = args[2].parse().expect("Task ID needs to be a number");
+        for task in &mut self.tasks {
+            if task.id == update_id {
+                task.status = "done".to_string();
+                task.updated_at = now_datetime();
+                break;
+            }
+        }
+        self.save();
     }
 }
 
@@ -26,84 +118,34 @@ fn create_id(tasks: &Vec<Task>) -> u32 {
     tasks.iter().map(|t| t.id + 1).max().unwrap_or(0)
 }
 
-fn task_create_datetime() -> String {
+fn now_datetime() -> String {
     Local::now().format("%Y-%m-%dT%H:%M:%S").to_string()
-}
-
-fn save_tasks(tasks: &Vec<Task>) {
-    let json = serde_json::to_string_pretty(&tasks).expect("failed to add task to list");
-    fs::write("tasks.json", json).expect("failed to write to file");
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    // create file if it doesnt exist
-    if !std::path::Path::new("tasks.json").exists() {
-        fs::write("tasks.json", "[]").expect("Could not create tasks.json");
-    }
-
-    // read file and then parse to json
-    let contents = fs::read_to_string("tasks.json").expect("cant read file");
-
-    let mut tasks: Vec<Task> = serde_json::from_str(&contents).expect("could not parse JSON");
+    let mut tasks = Tasks::load("tasks.json".to_string());
 
     // match for different options. list, add, etc.
     match args[1].as_str() {
         "list" => {
-            for task in &tasks {
-                /*check if there are more than one
-                argument before trying to access
-                index 2 */
-                if args.len() <= 2 || args[2] == task.status {
-                    println!("{} | {} | {}", task.id, task.description, task.status);
-                }
-            }
+            tasks.list(args);
         }
         "add" => {
-            let new_desc = args[2..].join(" ");
-            let new_task = create_task(&tasks, new_desc);
-            tasks.push(new_task);
-            save_tasks(&tasks);
+            tasks.create(args);
         }
         "update" => {
-            let task_update_desc: String = args[3..].join(" ");
-            let update_id: u32 = args[2].parse().expect("Task ID needs to be a number");
-            for task in &mut tasks {
-                if task.id == update_id {
-                    task.description = task_update_desc;
-                    task.updated_at = task_create_datetime();
-                    break;
-                }
-            }
-            save_tasks(&tasks);
+            tasks.update(args);
         }
         "remove" => {
-            let delete_id: u32 = args[2].parse().expect("Task ID needs to be a number");
-            tasks.retain(|task| task.id != delete_id);
-            save_tasks(&tasks);
+            tasks.delete(args);
         }
         "mark-in-progress" => {
-            let update_id: u32 = args[2].parse().expect("Task ID needs to be a number");
-            for task in &mut tasks {
-                if task.id == update_id {
-                    task.status = "in-progress".to_string();
-                    task.updated_at = task_create_datetime();
-                    break;
-                }
-            }
-            save_tasks(&tasks);
+            tasks.mark_in_progress(args);
         }
         "mark-done" => {
-            let update_id: u32 = args[2].parse().expect("Task ID needs to be a number");
-            for task in &mut tasks {
-                if task.id == update_id {
-                    task.status = "done".to_string();
-                    task.updated_at = task_create_datetime();
-                    break;
-                }
-            }
-            save_tasks(&tasks);
+            tasks.mark_done(args);
         }
 
         _ => {
